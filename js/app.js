89de8f5335175b94332ff163116ade12c2421f7b
mhha-17 <99782624+mhha-17@users.noreply.github.com>
2022-04-24 10:17:38 +0600

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,64 @@
+// var English = 82;
+
+// if(English >=80 && English <= 100) {
+//     document.write('English = A+');
+// }else if(English >= 70 && English <= 79){
+//     document.write('English = A');
+// }else if(English >= 60 && English <= 69){
+//     document.write('English = A-');
+// }else if(English >= 50 && English <= 59){
+//     document.write('English = B');
+// }else if( English >= 40 && English <= 49){
+//     document.write('English = C');
+// }else if(English >=33 && English <= 39){
+//     document.write('English = D');
+// } else{
+//     document.write('English = Failed');
+// }
+
+function sum(a,b,c){
+    document.write(a+ b+ c+ "<br>");
+}
+
+sum(5,7,8)
+
+function subtract(a,b){
+    document.write(a- b + "<br>");
+}
+
+subtract(89,6)
+
+function Multiplication(a,b) {
+    document.write( a * b + "<br>");
+}
+
+Multiplication(6,5)
+
+function division(a,b) {
+    document.write( a / b + "<br>");
+}
+
+division(78,5)
+
+function Modulus(a,b) {
+    document.write( a % b + "<br>");
+}
+
+Modulus(535,4)
+
+function increment(a) {
+    document.write( ++a + "<br>");
+}
+
+increment(7)
+
+
+function decrement(a) {
+    document.write( --a + "<br>");
+}
+
+decrement(11)
+
+
+
+
